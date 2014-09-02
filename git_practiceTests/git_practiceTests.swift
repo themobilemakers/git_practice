714c2c7dcf89246fc0c0eaae410d35f6c2abc090
Johnny Appleseed <engineering@mobilemakers.co>
2014-09-01 21:02:30 -0500

    first commit

---
+++ git_practiceTests/git_practiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  git_practiceTests.swift
+//  git_practiceTests
+//
+//  Created by Johnny Appleseed on 9/1/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class git_practiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
